c71e37ccb5f171feb137a38fb943965e95fbbdea
ageobab <agbabiki@gmail.com>
2014-10-08 20:21:46 -0500

    toggle exercise complete

--- gallery/script.js
+++ gallery/script.js
@@ -1,1 +1,25 @@
-// Add your code here
+// // Add your code here
+// $('.filter-button').on('click', function () {
+//   $(".mon");
+// });
+
+$('.filter-button').on('click', function(){
+$("." + this.id).toggle();
+$("#" + this.id).toggleClass('active');
+})
+
+
+
+
+// $('#monkey').click(function () {
+//     $('.gallery div').hide();
+//     $('.monkey').show();
+// });
+// $('#dog').click(function () {
+//     $('.gallery div').hide();
+//     $('.dog').show();
+// });
+// $('#cat').click(function () {
+//     $('.gallery div').hide();
+//     $('.cat').show()
+// });
